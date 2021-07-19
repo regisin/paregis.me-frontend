eb9a69b24aad0d46482c8564b9bec8af9bbf5fd2
Paulo Alexandre Regis <regisin@gmail.com>
2021-07-19 18:11:15 -0500

    aria-label

--- src/lib/components/LightBulb.svelte
+++ src/lib/components/LightBulb.svelte
@@ -2,7 +2,7 @@
     import { dark } from '$lib/components/store/global.js';
 </script>
 
-<button on:click={() => $dark = !$dark}>
+<button aria-label="Dark theme switch" on:click={() => $dark = !$dark}>
     {#if $dark}
     <svg width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
         <path stroke="none" d="M0 0h24v24H0z"/>
