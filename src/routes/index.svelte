cec8786bc0dbb28972da207bef4d90e930dee072
Paulo Alexandre Regis <regisin@gmail.com>
2021-10-12 14:59:02 -0500

    Remove prophet, sort articles by date

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,12 @@
 <script context="module">
 	export async function load({ page, fetch }) {
 		const res = await fetch(`https://paregisme.herokuapp.com/articles`);
-		const posts = await res.json();
+		let posts = await res.json();
+    posts = posts.sort(function(a,b){
+            // Turn your strings into dates, and then subtract them
+            // to get a value that is either negative, positive, or zero.
+            return new Date(b.published_at) - new Date(a.published_at);
+        });
 		return { props: { posts, page } };
 	}
 </script>
